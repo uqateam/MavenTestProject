7a503eec6ec41d0e96ac78b6fd3eca0de68276e9
tpamarthi <tpamarthi@994560APD034394.unicef.org>
2019-09-09 14:10:24 -0400

    1st Commit

---
+++ TestMaven/src/main/java/MavenTestProject/TestMaven/App.java
@@ -0,0 +1,18 @@
+package MavenTestProject.TestMaven;
+
+
+import org.openqa.selenium.WebDriver;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+   
+    }
+}
---
+++ TestMaven/src/test/java/MavenTestProject/TestMaven/SampleTest.java
@@ -0,0 +1,37 @@
+package MavenTestProject.TestMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver; 
+
+
+public class SampleTest {
+
+	@Test
+	public void test1()
+	
+	{
+		System.setProperty("webdriver.chrome.driver", "src\\resources\\chromedriver.exe");  
+	    WebDriver driver=new ChromeDriver();  
+		driver.navigate().to("http://www.google.com/");  
+		driver.manage().window().maximize(); 
+		String expectedname="Suuchi";
+		//String expectedname="Google";
+		String BrowserName = driver.getTitle();
+		System.out.println(BrowserName);
+		
+		if (BrowserName.contentEquals(expectedname)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+            driver.close();
+            Assert.fail();
+                     
+        }
+       
+		driver.close();
+	
+		
+	}
+}
